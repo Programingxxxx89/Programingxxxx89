da5dd4fbf3499e9c2a933e7b0ceb5581e23cbe07
Programingxxxx89 <abdrahim04112007@gmail.com>
2025-02-27 20:41:52 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const btnMore = document.getElementById("btn-more");
+
+btnMore.addEventListener("click", () => {
+    const sectionAbout = document.getElementById("about");
+    sectionAbout.style.display = "block";
+});
